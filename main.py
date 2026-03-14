4403bb4be1baa9862df5fc0e82a7b7c2a2c84750
santiago <santiagos.restrepo99@gmail.com>
2026-03-14 18:48:06 -0500

    se modificaron error del archivo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 # Asegurar que el directorio raíz esté en el path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from views.main_view import MainView
+from views.crud import MainView
 
 if __name__ == "__main__":
     app = MainView()
